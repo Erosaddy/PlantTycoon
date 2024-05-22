dd90bfcf773295ab529d2eb0028f0fb2e6b15720
Lee, Sangjun <zxz7648@gmail.com>
2024-05-22 14:26:01 +0900

    Chore: 시큐리티 테스트용 샘플 컨트롤러 제작 2024-05-22

--- planttycoon/src/main/java/kr/co/planttycoon/SampleController.java
+++ planttycoon/src/main/java/kr/co/planttycoon/SampleController.java
@@ -1,8 +1,33 @@
 package kr.co.planttycoon;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
 import lombok.extern.log4j.Log4j;
 
 @Log4j
+@RequestMapping("/sample/*")
+@Controller
 public class SampleController {
-
+	
+	@GetMapping("/all")
+	public void doAll() {
+		
+		log.info("do all can access everybody");
+	}
+	
+	@GetMapping("/member")
+	public void doMember() {
+		
+		log.info("logged in member");
+	}
+	
+	@GetMapping("/admin")
+	public void doAdmin() {
+		
+		log.info("admin only");
+	}
+	
 }
