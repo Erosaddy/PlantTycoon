ced1387ceba487860726f3e52aa05e5c44771c7f
Lee, Sangjun <zxz7648@gmail.com>
2024-05-22 17:53:02 +0900

    Test: Tested spring security using sample files 이상준 2024-05-22

--- planttycoon/src/main/java/kr/co/planttycoon/HomeController.java
+++ planttycoon/src/main/java/kr/co/planttycoon/HomeController.java
@@ -24,7 +24,7 @@ public class HomeController {
 	 */
 	@RequestMapping(value = "/", method = RequestMethod.GET)
 	public String home(Locale locale, Model model) {
-		logger.info("Funcking go home! The client locale is {}.", locale);
+		logger.info("The client locale is {}.", locale);
 		
 		Date date = new Date();
 		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
@@ -33,7 +33,7 @@ public class HomeController {
 		
 		model.addAttribute("serverTime", formattedDate );
 		
-		return "I want to go Home";
+		return "home";
 	}
 	
 }
