957466c4f4b63cf35d250ced958b9445b13367cf
pdh02189 <pdh02189@naver.com>
2024-06-04 17:38:05 +0900

    Feat: 온도/습도/조도 연동 진행중

--- planttycoon/src/main/java/kr/co/planttycoon/controller/MonitoringController.java
+++ planttycoon/src/main/java/kr/co/planttycoon/controller/MonitoringController.java
@@ -1,17 +1,30 @@
 package kr.co.planttycoon.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import kr.co.planttycoon.domain.MeasurementDTO;
+import kr.co.planttycoon.service.IMeasurementService;
 import lombok.extern.log4j.Log4j;
 
 @Log4j
 @Controller
 public class MonitoringController {
+	private final IMeasurementService service;
+	
+	@Autowired
+    public MonitoringController(IMeasurementService service) {
+       this.service = service;
+    }
+	
 	@GetMapping("/plant/monitoring")
-	public String plantstatus() {
+	public String plantstatus(Model model) {
+		// 최근 측정값을 가져와서 모델에 추가
+        MeasurementDTO latestMeasurement = service.getLatestMeasurement();
+        model.addAttribute("latestMeasurement", latestMeasurement);
+
         return "plant/monitoring";
 	}
 }
--- planttycoon/src/main/java/kr/co/planttycoon/service/impl/MeasurementServiceImpl.java
+++ planttycoon/src/main/java/kr/co/planttycoon/service/impl/MeasurementServiceImpl.java
@@ -6,10 +6,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import kr.co.planttycoon.domain.MeasurementDTO;
+import kr.co.planttycoon.domain.MemberDTO;
 import kr.co.planttycoon.mapper.MeasurementMapper;
 import kr.co.planttycoon.service.IMeasurementService;
+import lombok.extern.log4j.Log4j;
 
 @Service
+@Log4j
 public class MeasurementServiceImpl implements IMeasurementService {
 	private final MeasurementMapper mapper;
 	
@@ -18,14 +21,35 @@ public class MeasurementServiceImpl implements IMeasurementService {
        this.mapper = mapper;
     }
 	
+	// 일정 시간마다 모든 사용자에게 데이터 등록
+	@Override
+	public void saveMeasurement(MeasurementDTO measurement) {
+		List<MemberDTO> members = mapper.getAllMembers();
+        for (MemberDTO member : members) {
+            MeasurementDTO newMeasurement = new MeasurementDTO();
+            newMeasurement.setMemberId(member.getMemberId()); // 각 멤버의 아이디 설정
+            newMeasurement.setTemperature(measurement.getTemperature());
+            newMeasurement.setHumidity(measurement.getHumidity());
+            newMeasurement.setIlluminance(measurement.getIlluminance());
+            newMeasurement.setSoilMoisture(measurement.getSoilMoisture());
+            
+            log.info("Saving measurement for memberId: " + member.getMemberId());
+            
+            mapper.insertMeasurement(newMeasurement);
+        }
+	}
+	
+	// 가장 마지막 데이터
 	@Override
     public MeasurementDTO getLatestMeasurement() {
         return mapper.getLatestMeasurement();
     }
 	
+	// 최근 10개 데이터(차트)
 	@Override
 	public List<MeasurementDTO> getRecentMeasurements() {
 		return mapper.getRecentMeasurements();
 	}
+
 
 }
